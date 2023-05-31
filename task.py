32a4f2ddd53aaefa1c410b92bb5e776595f63f99
16ozcoldbru <osborna2@oregonstate.edu>
2023-05-31 13:41:43 -0700

    Added docstring header to task.py

--- task.py
+++ task.py
@@ -1,9 +1,14 @@
+"""This is a project for CS 362 exploring continuous integration
+
+Authors: Andrew Osborne, nextAuth, nextAuth
+Date: Spring Quarter 2023
+"""
 
 def conv_num(num_str):
     """Converts a string into a base 10 number
     
     Takes a string (num_str) and converts it into a base
-    10 number (conv_num), and returns that number.
+    10 number (conv_number), and returns that number.
 
     Returns:
     conv_num as an int
@@ -14,3 +19,7 @@ def conv_num(num_str):
 
     if len(num_str) == 0:
         return None
+
+    conv_number = 0
+
+    return conv_number
