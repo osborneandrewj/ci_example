5e5783ae9f94cb2f5735f61c4ecc9548cfb73bbc
johnvoliver <olive.joh@oregonstate.edu>
2023-06-03 15:02:24 -0400

    Implemented a function to determine whether or not a given year is a leap year. Wrote a number of tests to check functionality of this function.

--- task.py
+++ task.py
@@ -118,7 +118,30 @@ def my_datetime(num_sec):
 
     new_datetime = ''
 
+    num_days = num_sec // 86400
+
     return new_datetime
+
+
+def is_leap_year(year):
+    """
+    Takes an int representing a year and determines whether or not that
+    year is a leap_year
+
+    Returns:
+    A boolean
+    """
+
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 
 
 def conv_endian(num, endian='big'):
